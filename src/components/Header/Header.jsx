b488f42ea6e5c778e8ac9680c77936f510a448b9
he0o0nje <dudwogjswp@naver.com>
2023-10-12 15:44:09 +0900

    10-12(2)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,8 @@
 import * as style from "./HeaderStyle";
+import logo from "../../assets/images/header/logo.jpg";
+import mypage1 from "../../assets/images/header/mypage1.svg";
+import mypage2 from "../../assets/images/header/mypage2.svg";
+import mypage3 from "../../assets/images/header/mypage3.svg";
 
 function Header() {
   return (
@@ -39,6 +43,58 @@ function Header() {
             </li>
           </ul>
         </style.HeaderTop>
+        <style.HeaderBottom>
+          <style.LogoWrap>
+            <style.Link href="!">
+              <img src={logo} alt="" />
+            </style.Link>
+          </style.LogoWrap>
+          <style.MypageWrap>
+            <style.Link href="!">
+              <img src={mypage1} alt="" />
+            </style.Link>
+            <style.Link href="!">
+              <img src={mypage2} alt="" />
+            </style.Link>
+            <style.Link href="!">
+              <img src={mypage3} alt="" />
+            </style.Link>
+          </style.MypageWrap>
+          <style.TopCategory>
+            <ul>
+              <li>
+                <style.Link href="!">전 상품</style.Link>
+              </li>
+              <li>
+                <style.Link href="!">SALE</style.Link>
+              </li>
+              <li>
+                <style.Link href="!">100원딜</style.Link>
+              </li>
+              <li>
+                <style.Link href="!">식단관리</style.Link>
+              </li>
+              <li>
+                <style.Link href="!">간편식</style.Link>
+              </li>
+              <li>
+                <style.Link href="!">베이커리</style.Link>
+              </li>
+              <li>
+                <style.Link href="!">간식·음료</style.Link>
+              </li>
+              <li>
+                <style.Link href="!">분식·반찬</style.Link>
+              </li>
+              <li>
+                <style.Link href="!">홈트용품</style.Link>
+              </li>
+              <li>
+                <style.Link href="!">리빙용품</style.Link>
+              </li>
+            </ul>
+          </style.TopCategory>
+        </style.HeaderBottom>
       </style.Header>
     </>
   );
--- src/components/Header/HeaderStyle.js
+++ src/components/Header/HeaderStyle.js
@@ -28,10 +28,11 @@ export const HeaderTop = styled.div`
 export const boardList = styled.div`
   position: relative;
   .board_list {
+    display: none;
     position: absolute;
     top: 1.375rem;
     right: -1.9375rem;
-    padding: 0 20px;
+    padding: 0 1.25rem;
     border: 1px solid #999;
     background: #fff;
   }
@@ -50,11 +51,62 @@ export const Link = styled.a`
   display: flex;
   align-items: center;
   .arrRgt {
-    width: 5px;
-    height: 5px;
-    margin-left: 5px;
-    border-left: 1px solid #000;
-    border-bottom: 1px solid #000;
+    width: 0.3125rem;
+    height: 0.3125rem;
+    margin-left: 0.3125rem;
+    border-left: 0.0625rem solid #000;
+    border-bottom: 0.0625rem solid #000;
     transform: rotate(-135deg);
+  }
+`;
+
+export const HeaderBottom = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+  height: auto;
+  margin-top: 1.875rem;
+`;
+
+export const LogoWrap = styled.div`
+  order: 1;
+  flex-basis: 50%;
+  flex-shrink: 0;
+  height: 3.75rem;
+  img {
+    max-width: 25rem;
+    max-height: 3.75rem;
+  }
+`;
+
+export const MypageWrap = styled.div`
+  order: 2;
+  display: flex;
+  justify-content: flex-end;
+  flex-basis: 50%;
+  flex-shrink: 0;
+  margin-left: auto;
+  a {
+    margin-left: 1.875rem;
+  }
+`;
+
+export const TopCategory = styled.div`
+  order: 3;
+  display: flex;
+  width: 100%;
+  ul {
+    display: flex;
+    height: 5rem;
+  }
+  li {
+    padding-right: 3.125rem;
+  }
+  a {
+    font-size: 1rem;
+    line-height: 4.875rem;
+    color: #1a1a1a;
+    letter-spacing: -0.0563rem;
   }
 `;
