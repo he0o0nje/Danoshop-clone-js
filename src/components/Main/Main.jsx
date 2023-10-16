29606b1bcf800acc00d2862e1bbf3e6a364a65a5
he0o0nje <dudwogjswp@naver.com>
2023-10-16 14:56:53 +0900

    10-16(2)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -51,6 +51,28 @@ function Main() {
             </Carousel.Item>
           </Carousel>
         </style.MainBanner>
+        <style.MainCont>
+          <style.MainTit>
+            <p>아침 7시, 통곡물&고단백 아침식단</p>
+            <p>준비하는데 3분컷, 간편하게 영양만점 하루 시작!</p>
+          </style.MainTit>
+          <style.MainProdList>
+            <ul>
+              <li>
+                <style.MainProd>
+                  <div className="prod_thumb">
+                    <a href="!">
+                      <img src="" alt="" />
+                    </a>
+                    <div className="icon_box"></div>
+                    <span></span>
+                  </div>
+                  <div className="prod_desc"></div>
+                </style.MainProd>
+              </li>
+            </ul>
+          </style.MainProdList>
+        </style.MainCont>
       </style.Main>
     </>
   );
