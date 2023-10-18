654b11801fb0dbd56df24f5aabc68cbe3b980ae7
he0o0nje <dudwogjswp@naver.com>
2023-10-18 09:40:02 +0900

    10-18

--- src/components/Main/Banner.jsx
+++ src/components/Main/Banner.jsx
@@ -1,12 +1,5 @@
 import * as style from "./BannerStyle";
 import Carousel from "react-bootstrap/Carousel";
-import mbn1 from "../../assets/images/banner/01.png";
-import mbn2 from "../../assets/images/banner/02.jpeg";
-import mbn3 from "../../assets/images/banner/03.png";
-import mbn4 from "../../assets/images/banner/04.png";
-import mbn5 from "../../assets/images/banner/05.png";
-import mbn6 from "../../assets/images/banner/06.png";
-import mbn7 from "../../assets/images/banner/07.png";
 
 function Banner() {
   return (
@@ -15,37 +8,37 @@ function Banner() {
         <Carousel fade controls={false}>
           <Carousel.Item interval={1500}>
             <a href="!">
-              <img src={mbn1} alt="" />
+              <img src="img/banner/01.png" alt="" />
             </a>
           </Carousel.Item>
           <Carousel.Item interval={1500}>
             <a href="!">
-              <img src={mbn2} alt="" />
+              <img src="img/banner/02.jpeg" alt="" />
             </a>
           </Carousel.Item>
           <Carousel.Item interval={1500}>
             <a href="!">
-              <img src={mbn3} alt="" />
+              <img src="img/banner/03.png" alt="" />
             </a>
           </Carousel.Item>
           <Carousel.Item interval={1500}>
             <a href="!">
-              <img src={mbn4} alt="" />
+              <img src="img/banner/04.png" alt="" />
             </a>
           </Carousel.Item>
           <Carousel.Item interval={1500}>
             <a href="!">
-              <img src={mbn5} alt="" />
+              <img src="img/banner/05.png" alt="" />
             </a>
           </Carousel.Item>
           <Carousel.Item interval={1500}>
             <a href="!">
-              <img src={mbn6} alt="" />
+              <img src="img/banner/06.png" alt="" />
             </a>
           </Carousel.Item>
           <Carousel.Item interval={1500}>
             <a href="!">
-              <img src={mbn7} alt="" />
+              <img src="img/banner/07.png" alt="" />
             </a>
           </Carousel.Item>
         </Carousel>
