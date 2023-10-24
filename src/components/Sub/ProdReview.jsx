94f7d46422831f82ac668f4efb730b58af4a78fc
he0o0nje <dudwogjswp@naver.com>
2023-10-24 15:01:28 +0900

    10-24(1)

--- src/components/Sub/ProdReview.jsx
+++ src/components/Sub/ProdReview.jsx
@@ -6,6 +6,17 @@ function ProdReview() {
     <>
       <style.ProdReview>
         <DetailTab02></DetailTab02>
+        <style.ReviewWrap>
+          <div className="header">
+            <div className="title_wrap">
+              <span className="title"> REVIEW </span>
+              <span className="review_count"> (2,020) </span>
+            </div>
+            <div className="all_review">
+              <a href="!">전체 상품 리뷰 보기</a>
+            </div>
+          </div>
+        </style.ReviewWrap>
       </style.ProdReview>
     </>
   );
--- src/components/Sub/ProdReviewStyle.js
+++ src/components/Sub/ProdReviewStyle.js
@@ -3,3 +3,41 @@ import styled from "styled-components";
 export const ProdReview = styled.div`
   padding-top: 15rem;
 `;
+
+export const ReviewWrap = styled.div`
+  width: 100%;
+  margin: 3.2rem 0;
+  padding: 0 3.2rem 4.4rem;
+  font-size: 1.2rem;
+  background: transparent;
+  font-family: "돋움", Dotum, Helvetica, "Apple SD Gothic Neo", Sans-serif;
+  .header {
+    display: flex;
+    border-bottom: 0.2rem solid #14161a;
+    padding: 1.6rem 0;
+    .title_wrap {
+      flex-grow: 1;
+      line-height: 3rem;
+      font-size: 2rem;
+      color: #14161a;
+      .title {
+        margin-right: 0.4rem;
+      }
+      .review_count {
+        letter-spacing: 0.05rem;
+        font-weight: bold;
+      }
+    }
+    .all_review {
+      display: flex;
+      height: 2rem;
+      margin-top: 0.3rem;
+      a {
+        white-space: nowrap;
+        font-size: 1.6rem;
+        color: #707680;
+        padding: 0 0 1.6rem 1.2rem;
+      }
+    }
+  }
+`;
