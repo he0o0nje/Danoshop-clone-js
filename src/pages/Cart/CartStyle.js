e138eabf51a77ef76dcb6fd76ba5ad44d67dc995
he0o0nje <dudwogjswp@naver.com>
2023-10-31 12:26:17 +0900

    10-31

--- src/pages/Cart/CartStyle.js
+++ src/pages/Cart/CartStyle.js
@@ -35,3 +35,50 @@ export const Cart = styled.div`
     }
   }
 `;
+
+export const Help = styled.div`
+  max-width: 1420px;
+  width: 92%;
+  margin: 5rem auto 0;
+  line-height: 1.8rem;
+  h3 {
+    display: block;
+    margin: 0 0 2rem;
+    font-size: 1.6rem;
+    font-weight: bold;
+    color: #000;
+  }
+  .inner {
+    padding: 2rem;
+    border: 0.1rem solid #e5e5e5;
+    border-top: 0.2rem solid #1a1a1a;
+    h4 {
+      margin: 22px 0 -4px;
+      color: #000;
+      font-size: 12px;
+      font-weight: normal;
+      &:first-child {
+        margin-top: 0;
+      }
+    }
+    ul {
+      margin-top: 1.5rem;
+      li {
+        padding-left: 1.2rem;
+        color: #6d6d6d;
+        font-size: 12px;
+        line-height: 21px;
+        &::before {
+          content: "";
+          display: inline-block;
+          width: 3px;
+          height: 3px;
+          margin: 7px 9px 0 -12px;
+          vertical-align: top;
+          border-radius: 50%;
+          background: #6d6d6d;
+        }
+      }
+    }
+  }
+`;
